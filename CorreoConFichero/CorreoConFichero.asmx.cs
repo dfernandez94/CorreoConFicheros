7844ab87da46a3d1fc31f52807474911d2382f8f
David Fernandez <dfernandez@afirmatica.com>
2016-06-15 12:05:23 +0200

    generarFichero, obtenerFichero y enviarEmail

---
+++ CorreoConFichero/CorreoConFichero.asmx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CorreoConFichero
+{
+    /// <summary>
+    /// Descripción breve de CorreoConFichero
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CorreoConFichero : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string generarFichero()
+        {
+            string[] albaranes = { "144292#220#220#0#542595#10/05/16#09:15#13/05/16#1#60#100",
+                "144292#220#220#0#300079#10/05/16#09:15#13/05/16#1#150#20",
+                "144292#220#220#0#300109#10/05/16#09:15#13/05/16#1#10#30000" };
+
+            try {
+                System.IO.File.WriteAllLines(@"C:\Albaranes\WriteLines.txt", albaranes);
+                return "Fichero generado";
+            }
+            catch(Exception e)
+            {
+                return "No se ha generado el fichero "+e.Message;
+            }
+
+           
+        }
+
+        [WebMethod]
+        public Byte[] obtenerFichero()
+        {
+            string strdocPath;
+            strdocPath = @"C:\Albaranes\WriteLines.txt";
+
+            FileStream objfilestream = new FileStream(strdocPath, FileMode.Open, FileAccess.Read);
+            int len = (int)objfilestream.Length;
+            Byte[] documentcontents = new Byte[len];
+            objfilestream.Read(documentcontents, 0, len);
+            objfilestream.Close();
+
+            return documentcontents;
+        }
+
+        /*Leer el albaran en el cliente*/
+
+        /*
+        FileStream objfilestream = new FileStream(sFile, FileMode.Open, FileAccess.Read);
+        int len = (int)objfilestream.Length;
+        Byte[] mybytearray = new Byte[len];
+        objfilestream.Read(mybytearray, 0, len);
+        localhost.Service1 myservice = new localhost.Service1();
+        myservice.SaveDocument(mybytearray, sFile.Remove(0, sFile.LastIndexOf("\\") + 1));
+        objfilestream.Close();
+        */
+        
+        [WebMethod]
+        public string enviarEmail()
+        {
+            
+            List<string> lstArchivos = new List<string>();
+            lstArchivos.Add(@"C:\Albaranes\WriteLines.txt");
+            
+            Mail oMail = new Mail("pruebaa <davidtigrex@gmail.com>", "dfernandez@afirmatica.com","asunto", "mensaje", lstArchivos);
+            
+            if (oMail.enviar())
+            {
+                return "se envio el mail";
+            }
+            else
+            {
+                return "no se envio el mail: " + oMail.error;
+            }
+
+
+        }
+    }
+}
---
+++ CorreoConFichero/Mail.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorreoConFichero
+{
+    class Mail
+    {
+
+        /*Email*/
+        public String de { set; get; }
+        public String para { set; get; }
+        public String mensaje { set; get; }
+        public String asunto { set; get; }
+        public List<string> archivos { set; get; }
+
+        /*SMTP*/
+        public String usuario { set; get; }
+        public String contra { set; get; }
+        public bool habilitarSsl { set; get; }
+        public String smtp { set; get; }
+        public int puerto { set; get; }
+
+        public System.Net.Mail.MailMessage email { set; get; }
+
+        public string error { set; get; }
+
+        public Mail(string de, string para, string asunto, string mensaje, List<string> archivos = null)
+        {
+            this.de = de;
+            this.para = para;
+            this.mensaje = mensaje;
+            this.asunto = asunto;
+            this.archivos = archivos;
+
+            usuario = ConfigurationManager.AppSettings["correo.usuario"];
+            contra = ConfigurationManager.AppSettings["correo.contra"];
+            habilitarSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["correo.ssl"]);
+            smtp = ConfigurationManager.AppSettings["correo.smtp"];
+            puerto = Convert.ToInt32(ConfigurationManager.AppSettings["correo.puerto"]);
+        }
+
+        public bool enviar()
+        {
+
+            if (de.Trim().Equals("") || para.Trim().Equals("") || asunto.Trim().Equals(""))
+            {
+                error = "El mail, el asunto y el mensaje son obligatorios";
+                return false;
+            }
+
+            try
+            {
+                email = new System.Net.Mail.MailMessage(de, para, asunto, mensaje);
+
+                if (archivos != null)
+                {
+                    foreach (string archivo in archivos)
+                    {
+                        //comprobamos si existe el archivo y lo agregamos a los adjuntos
+                        if (System.IO.File.Exists(@archivo))
+                            email.Attachments.Add(new Attachment(@archivo));
+
+                    }
+                }
+
+                email.IsBodyHtml = true;
+                email.From = new MailAddress(de); 
+
+                System.Net.Mail.SmtpClient smtpMail = new System.Net.Mail.SmtpClient(smtp);
+
+                smtpMail.EnableSsl = habilitarSsl;
+                smtpMail.UseDefaultCredentials = false; 
+                smtpMail.Host = smtp;
+                smtpMail.Port = puerto; 
+                smtpMail.Credentials = new System.Net.NetworkCredential(usuario, contra); 
+
+                smtpMail.Send(email);
+                smtpMail.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                error = "Ocurrio un error: " + ex.Message;
+                return false;
+            }
+
+            
+
+        }
+    }
+}
